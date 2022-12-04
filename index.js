7b1898fee81ef48fc40f1fd7c9a70f3be3593beb
vinayak <khandekarvinayak41@gmail.com>
2022-12-04 22:00:34 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const cardRouter = require("./src/routes/card");
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 mongoose
   .connect(config.MONGODB_URI, {
